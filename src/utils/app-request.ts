6dddca060bd17625ab2dec8176478100255b7cff
rashidmehmood <rashidpadda145@gmail.com>
2025-05-14 13:07:34 +0400

    remove comments

--- src/utils/app-request.ts
+++ src/utils/app-request.ts
@@ -2,6 +2,6 @@ import { Request } from 'express';
 import { User } from '../users/entities/users.model';
 
 export interface AppRequest extends Request {
-  user?: User; // Attach the User entity to the request object
+  user?: User; 
   [key: string]: any; // Allow additional custom properties
 }
